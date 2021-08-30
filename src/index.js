f8d4486ee996d33d5b329c4fec19efa82c2a9588
Taras-Kolomiiets <taraskolomiiets8991@gmail.com>
2021-08-30 21:21:08 +0300

    Finish Task 13

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import ImageTpl from './templates/image.hbs';
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
+import Notify from 'simple-notify';
+import 'simple-notify/dist/simple-notify.min.css';
+
 const refs = {
   searchForm: document.querySelector('.search-form'),
   imagesContainer: document.querySelector('.gallery'),
@@ -23,10 +26,26 @@ loadMoreBtn.refs.button.addEventListener('click', fetchImages);
 function onSearch(e) {
   e.preventDefault();
 
-  imagesApiService.query = e.currentTarget.elements.query.value;
+  imagesApiService.query = e.currentTarget.elements.query.value.trim();
 
   if (imagesApiService.query === '') {
-    return alert('Введи что-то нормальное');
+    return new Notify({
+      status: 'warning',
+      title: 'Enter a search query!',
+      text: 'Write what you want to find.',
+      effect: 'fade',
+      speed: 300,
+      customClass: '',
+      customIcon: '',
+      showIcon: true,
+      showCloseButton: true,
+      autoclose: true,
+      autotimeout: 2000,
+      gap: 20,
+      distance: 20,
+      type: 1,
+      position: 'right top'
+    });
   }
 
   loadMoreBtn.show();
@@ -39,8 +58,29 @@ function fetchImages() {
   loadMoreBtn.disable();
   imagesApiService.fetchImages().then( images  => {
     appendImagesMarkup(images);
-    console.log(`images`, images)
     loadMoreBtn.enable();
+    if (images.totalHits === 0) {
+      loadMoreBtn.hide();
+      return new Notify({
+        status: 'error',
+        title: 'Oops!',
+        text: 'Sorry, there are no images matching your search query.',
+        effect: 'fade',
+        speed: 300,
+        customClass: '',
+        customIcon: '',
+        showIcon: true,
+        showCloseButton: true,
+        autoclose: true,
+        autotimeout: 2000,
+        gap: 20,
+        distance: 20,
+        type: 1,
+        position: 'right top'
+      });
+    }
+  }).catch(error => {
+    console.log(`error`, error)
   });
 }
 
